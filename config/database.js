621f4f340b9087c3074af6e8eaa1de7e8272b9a9
WholesomeAhki <Victor.D.Jackson@gmail.com>
2022-11-07 23:59:28 -0500

    first draft of breaking and making my demo must fix routes

---
+++ config/database.js
@@ -0,0 +1,6 @@
+// config/database.js
+module.exports = {
+
+    'url' : 'mongodb+srv://VictorAzizJ:demo@cluster0.q2ojb.mongodb.net/ourchive?retryWrites=true&w=majority', 
+    'dbName': 'ourchive'
+};
---
+++ public/main.js
@@ -0,0 +1,81 @@
+/*var thumbUp = document.getElementsByClassName("fa-thumbs-up");
+var trash = document.getElementsByClassName("fa-trash-o");
+//make listener for thumbs down//
+var thumbDown = document.getElementsByClassName("fa-thumbs-down")
+
+Array.from(thumbUp).forEach(function(element) {
+      element.addEventListener('click', function(){
+        const name = this.parentNode.parentNode.childNodes[1].innerText
+        const msg = this.parentNode.parentNode.childNodes[3].innerText
+        
+        fetch('interactions', {
+          method: 'put',
+          headers: {'Content-Type': 'application/json'},
+          body: JSON.stringify({
+            'name': name,
+            'msg': msg,
+            'thumbUp': true
+          })
+        })
+        .then(response => {
+          if (response.ok) return response.json()
+        })
+        .then(data => {
+          console.log(data)
+          window.location.reload(true)
+        })
+      });
+});
+//make thumbs down function//
+Array.from(thumbDown).forEach(function(element){
+  element.addEventListener('click', function(){
+    const name = this.parentNode.parentNode.childNodes[1].innerText
+    const msg = this.parentNode.parentNode.childNodes[3].innerText
+    
+    fetch('interactions', {
+      method: 'put',
+      headers: {'Content-Type' : 'application/json'},
+      body: JSON.stringify({
+        'name' : name,
+        'msg' : msg,
+        'thumbUp' : false
+      })
+      
+
+
+    })
+    .then(response => {
+      if (response.ok) return response.json()
+    })
+    .then(data => {
+      console.log(data)
+      window.location.reload(true)
+    })
+
+  })
+
+
+
+
+
+})
+
+Array.from(trash).forEach(function(element) {
+      element.addEventListener('click', function(){
+        const name = this.parentNode.parentNode.childNodes[1].innerText
+        const msg = this.parentNode.parentNode.childNodes[3].innerText
+        fetch('interactions', {
+          method: 'delete',
+          headers: {
+            'Content-Type': 'application/json'
+          },
+          body: JSON.stringify({
+            'name': name,
+            'msg': msg
+          })
+        }).then(function (response) {
+          window.location.reload()
+          //maybe comment out so available to look at object
+        })
+      });
+});*/
