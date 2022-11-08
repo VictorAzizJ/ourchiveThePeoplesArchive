cd672872028bd38fde4ed7aa1f3e3b846c0fa7f3
WholesomeAhki <Victor.D.Jackson@gmail.com>
2022-11-08 11:57:37 -0500

    fixed now time to add project elements and functional registeration for food pantry

--- config/database.js
+++ config/database.js
@@ -1,6 +1,6 @@
 // config/database.js
 module.exports = {
 
-    'url' : 'mongodb+srv://VictorAzizJ:demo@cluster0.q2ojb.mongodb.net/ourchive?retryWrites=true&w=majority', 
+    'url' : 'mongodb+srv://VictorAzizJ:demo@cluster0.ed64pxn.mongodb.net/ourchive?retryWrites=true&w=majority', 
     'dbName': 'ourchive'
 };
